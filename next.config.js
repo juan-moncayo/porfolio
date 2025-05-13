c9a5f851cd9035837fc3522cea7137d92ad2cfce
paycongroup <paycongroup@gmail.com>
2025-05-13 02:58:17 -0500

    fix14

--- next.config.js
+++ next.config.js
@@ -5,6 +5,8 @@ const nextConfig = {
     unoptimized: true,
   },
   typescript: {
+    // ⚠️ Dangerously allow production builds to successfully complete even if
+    // your project has type errors.
     ignoreBuildErrors: true,
   },
   eslint: {
