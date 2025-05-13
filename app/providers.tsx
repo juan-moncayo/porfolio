34139a6d2258ae8ec1223cd10f4f38a6f8222d00
paycongroup <paycongroup@gmail.com>
2025-05-13 02:22:18 -0500

    fix6

--- app/providers.tsx
+++ app/providers.tsx
@@ -1,11 +1,13 @@
 "use client"
 
+import type React from "react"
+
 import { ThemeProvider as NextThemesProvider } from "next-themes"
 import { NextUIProvider } from "@nextui-org/react"
 
 export function Providers({ children }: { children: React.ReactNode }) {
   return (
-    <NextThemesProvider attribute="class" defaultTheme="dark">
+    <NextThemesProvider attribute="class" defaultTheme="dark" enableSystem={true}>
       <NextUIProvider>{children}</NextUIProvider>
     </NextThemesProvider>
   )
--- next.config.js
+++ next.config.js
@@ -1,6 +1,25 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  /* config options here */
-};
+  output: "standalone",
+  swcMinify: true,
+  reactStrictMode: true,
+  poweredByHeader: false,
+  eslint: {
+    ignoreDuringBuilds: true,
+  },
+  typescript: {
+    ignoreBuildErrors: true,
+  },
+  images: {
+    domains: ["localhost"],
+    remotePatterns: [
+      {
+        protocol: "https",
+        hostname: "**",
+      },
+    ],
+    unoptimized: true,
+  },
+}
 
-module.exports = nextConfig;
+module.exports = nextConfig
