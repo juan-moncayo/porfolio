969a401e277bb86eb806c38528b9799145bb7b15
paycongroup <paycongroup@gmail.com>
2025-05-13 02:44:31 -0500

    fix11

--- next.config.js
+++ next.config.js
@@ -11,6 +11,15 @@ const nextConfig = {
     ignoreDuringBuilds: true,
   },
   swcMinify: false,
+  // Añadir alias para resolver las rutas incorrectas
+  webpack: (config) => {
+    config.resolve.alias = {
+      ...config.resolve.alias,
+      "@/app/components/Header": require("path").resolve("./components/Header"),
+      "@/app/components/Footer": require("path").resolve("./components/Footer"),
+    }
+    return config
+  },
 }
 
 module.exports = nextConfig
